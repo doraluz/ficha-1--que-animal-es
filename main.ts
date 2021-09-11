b9dcaa9aec35404d788265866c1eb471dfde1864
doraluz <33816619+doraluz@users.noreply.github.com>
2021-09-11 12:07:17 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,67 @@
+basic.forever(function () {
+    basic.showLeds(`
+        . # # . .
+        # # # # .
+        # # # # .
+        . # # . .
+        . . . . .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        # # . . .
+        . # . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(3000)
+    basic.showLeds(`
+        # . . . .
+        . # . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(2000)
+    basic.showLeds(`
+        # . . . .
+        # . . . .
+        # . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(3000)
+    basic.showLeds(`
+        # # . . .
+        # . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(3000)
+    basic.showLeds(`
+        . # . . .
+        # . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(4000)
+    basic.showLeds(`
+        # # . . .
+        . # . . .
+        # . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(3000)
+    basic.showLeds(`
+        . # # . .
+        # # # # .
+        # # # # .
+        . # # . .
+        . . . . .
+        `)
+    basic.pause(1000)
+    basic.clearScreen()
+})
